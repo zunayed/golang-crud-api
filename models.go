a7b9e2234eab758c1cdac191b8ac0fb21d69bccd
Zunayed Ali Morsalin <zunayed@gmail.com>
2017-11-25 01:32:45 -0500

    adding create and delete db functions

--- models.go
+++ models.go
@@ -19,3 +19,25 @@ func getProduct(DB *gorm.DB, id int) (Product, error) {
 
 	return product, nil
 }
+
+func createProduct(DB *gorm.DB, code string, price uint) (uint, error) {
+	p := Product{Code: code, Price: price}
+
+	err := DB.Create(&p).Error
+	if err != nil {
+		return 0, err
+	}
+
+	return p.ID, nil
+}
+
+func deleteProduct(DB *gorm.DB, price uint) error {
+	var p Product
+	DB.Where("id = ?", price).First(&p)
+	err := DB.Delete(&p).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
